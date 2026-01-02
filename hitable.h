1ca956eb633b64cfe0028476b259831030ed5fe2
ericwernstrom <eric.wernstrom@gmail.com>
2026-01-02 13:58:07 +0100

    branchtest

--- hitable.h
+++ hitable.h
@@ -13,9 +13,10 @@ struct hit_record
     material *mat_ptr;
 };
 
-class hitable  {
-    public:
-        __device__ virtual bool hit(const ray& r, float t_min, float t_max, hit_record& rec) const = 0;
+class hitable
+{
+public:
+    __device__ virtual bool hit(const ray &r, float t_min, float t_max, hit_record &rec) const = 0;
 };
 
 #endif
